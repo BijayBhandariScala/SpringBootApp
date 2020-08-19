a516e26d2f83caeb865723fb844f9e5bf03c0ff5
bijay <bijay.bhandari007@gmail.com>
2020-08-19 10:32:32 -0400

    Added Cloud server config module

---
+++ CloudServerConfig/src/main/java/com/bijay/springboot/practise/CloudServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.bijay.springboot.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerConfigApplication.class, args);
+	}
+
+}
