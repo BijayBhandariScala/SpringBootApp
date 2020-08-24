52d08569488118395bce7c47f3db7a86eddb1d86
bijay <bijay.bhandari007@gmail.com>
2020-08-24 17:00:41 -0400

    Updated project.

--- CloudConfig/src/main/java/com/bijay/springboot/practise/CloudConfigApplication.java
+++ CloudConfig/src/main/java/com/bijay/springboot/practise/CloudConfigApplication.java
@@ -2,12 +2,14 @@ package com.bijay.springboot.practise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class CloudServerConfigApplication {
+@EnableConfigServer
+public class CloudConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudServerConfigApplication.class, args);
+		SpringApplication.run(CloudConfigApplication.class, args);
 	}
 
 }
