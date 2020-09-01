bca9a4c67779c6c940707eb4ca0a7100b091e154
bijay <bijay.bhandari007@gmail.com>
2020-09-01 18:24:14 -0400

    Added properties
    Used ResponseEntity

--- SpringBootApplication/src/main/java/com/bijay/springboot/practise/RoomController.java
+++ SpringBootApplication/src/main/java/com/bijay/springboot/practise/RoomController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,11 +19,15 @@ public class RoomController {
 	@Autowired
 	private RoomRepository roomRepository;
 
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Room> findAll(@RequestParam(name = "roomNumber", required = false) String roomNumber) {
+	public ResponseEntity<List<Room>> findAll(@RequestParam(name = "roomNumber", required = false) String roomNumber) {
 		if (StringUtils.isNotEmpty(roomNumber)) {
-			return Collections.singletonList(this.roomRepository.findByRoomNumber(roomNumber));
+			return new ResponseEntity(Collections.singletonList(this.roomRepository.findByRoomNumber(roomNumber)), HttpStatus.OK);
 		}
-		return (List<Room>) this.roomRepository.findAll();
+		return new ResponseEntity((List<Room>) this.roomRepository.findAll(), HttpStatus.OK);
 	}
+
+
+
 }
